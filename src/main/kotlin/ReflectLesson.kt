364607f79e95b6f452db8e15c141f3917727cee1
= <=>
2022-02-18 21:25:01 +0800

    feature:Reflection for KType

--- src/main/kotlin/ReflectLesson.kt
+++ src/main/kotlin/ReflectLesson.kt
@@ -1,4 +1,6 @@
 import java.lang.reflect.*
+import kotlin.reflect.KClass
+import kotlin.reflect.KTypeParameter
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.jvm.javaMethod
 
@@ -18,23 +20,31 @@ fun main() {
     Tool::class.declaredFunctions.forEach {
         val returnType = it.returnType
         val classifier = returnType.classifier
-//        println(" $classifier ${classifier is KTypeParameter}")
         val arguments = returnType.arguments
-        println(arguments)
+        if (classifier is KTypeParameter) {
+            println("type --> $returnType," +
+                    "classifier upperBounds --> ${classifier.upperBounds}, " +
+                    "classifier variance --> ${classifier.variance}"+
+                    ", arguments --> $arguments")
+        }
+        if(classifier is KClass<*>){
+            println("type --> $returnType, classifier members ${classifier.members},arguments --> $arguments")
+        }
+
     }
 
 
 }
 
 
-interface Tool<R, T,  W:Number> {
+interface Tool<R, out T, W : Number> {
     fun adapt(r: R): T //TypeVariable
-    fun adapt1(r: R): Array<T>//GenericArrayType
+    fun adapt1(r: R): Array<out T>//GenericArrayType
     fun adapt2(r: R): List<*> //ParameterizedType
     fun adapt3(r: R): Array<String>
     fun adapt4(r: R): String //RawType
-    fun <K>adapt5(r:R):K
-    fun <K>adapt6(r:R):W
+    fun <K> adapt5(r: R): K
+    fun <K> adapt6(r: R): W
 }
 
 //kotlin 不再支持WildcardType,但是有通配符号*
